6b66e6f95b2fa46fbbce88e540fdd83f1ffe8d85
aryaantony92 <97134765+aryaantony92@users.noreply.github.com>
2023-04-14 15:42:27 +0200

    Update .php-cs-fixer.dist.php
    Co-authored-by: robertSt7 <104770750+robertSt7@users.noreply.github.com>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,7 +1,10 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->in([__DIR__ . '/src'])
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/config'
+    ])
 
     ->exclude([
         __DIR__ . '/tests/_output',
